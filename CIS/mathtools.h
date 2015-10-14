35e0e83c11951c15192f96a6e3c154fcaba6e76e
Èric Pairet Artau <ericpairet@gmail.com>
2015-10-14 23:55:53 +0200

    Minor changes

--- CIS/mathtools.h
+++ CIS/mathtools.h
@@ -11,8 +11,8 @@ public:
     ~CMaths(){}
 
     // Function that returns the minimum number of three
-    //static inline double minimumOfThree( const double &a, const double &b, const double &c){return a<b?a<c?a:c:b<c?b:c;}
-    // static inline int minimumOfThree( const int &a, const int &b, const int &c){return a<b?a<c?a:c:b<c?b:c;}
+    static inline double minimumOfThree( const double &a, const double &b, const double &c){return a<b?a<c?a:c:b<c?b:c;}
+    static inline int minimumOfThree( const int &a, const int &b, const int &c){return a<b?a<c?a:c:b<c?b:c;}
     // Function that returns the maximum number of three
     static inline double maximumOfThree( const double &a, const double &b, const double &c){return a>b?a>c?a:c:b>c?b:c;}
     static inline int maximumOfThree( const int &a, const int &b, const int &c){return a>b?a>c?a:c:b>c?b:c;}
