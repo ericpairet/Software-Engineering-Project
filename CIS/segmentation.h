e8cecfa4d41c148ce310d684877fc4c96c8fd264
ericpairet <ericpairet@gmail.com>
2015-11-07 16:43:10 +0100

    Segementation code cleaned

--- CIS/segmentation.h
+++ CIS/segmentation.h
@@ -2,17 +2,19 @@
 #define CSEGMENTATION_H
 
 #include "widgets.h"
+
+#include <ctime>
 #include <iostream>
+
 #include <Eigen/Dense>
-#include <Eigen/LU>
-//#include <Eigen/Cholesky>
 #include <Eigen/Core>
-#include <Eigen/SparseCholesky>
+#include <Eigen/Sparse>
 #include <Eigen/SparseCore>
-#include <opencv2/opencv.hpp>
+#include <Eigen/SparseCholesky>
+
+#include "opencv2/opencv.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/imgproc/types_c.h"
-#include <ctime>                            // Only for debuging purposes
 
 using namespace std;
 using namespace Eigen;
@@ -21,129 +23,92 @@ using namespace cv;
 class CSegmentation : public QObject
 {
     Q_OBJECT
+
 public:
-    // Constructor
+
     CSegmentation( CToolsWidget *_t, CMonitorWidget *_m ) : QObject() {
         tools = _t;
         monitor = _m;
     }
-
-    // Destructor
     ~CSegmentation() {}
 
 public slots:
-    void run()
-    {
-        // Variables only for performance purposes
-        time_t globalstart, tstart, tend;
 
+    void run() {
+
+        //All this should be moved out from this CLASS
+// *****************************************************************************************************************************************************************
         // Load image to be computed ( m x n )
         // Mat I = QPixmapToCvMat( *image);
         Mat I = imread( tools->imagePath.toStdString().c_str() , CV_32FC3 );
         //I = img2grey(I);
 
         // Take m and n image size
-        int m = I.rows;
-        int n = I.cols;
+        //int m = I.rows;
+        //int n = I.cols;
 
         // Check if there is an image
-        if ( ( m * n ) == 0 ) {
+        /*if ( ( m * n ) == 0 ) {
             cout << "No image" << endl;
             return;
-        }
+        }*/
+
+        // Initialize tunning constants
+        double betta = 0.005;
+        double sigma = 0.1;
+// *****************************************************************************************************************************************************************
+
+        // Save segmentation start time
+        const clock_t t_start = clock();
+
+        // Take m and n image size
+        int m = I.rows;
+        int n = I.cols;
 
         // Initialize background and foreground labels
         double xb = 1;
         double xf = 0;
 
-        // Check xb > xf
+        // Check xb > xf                                            // if xb and xf are hardcoded this condition can be removed
         if ( xb < xf ) {
             cout << "Background level must be greater than the foreground one" << endl;
             return;
         }
 
-        // Initialize tunning constants
-        double betta = 0.005;
-        double sigma = 0.1;
-
-        globalstart = time(0);                                                              // ***
-
-        tstart = time(0);                                                                   // ***
         // Compute the Graph Laplacian Matrix (L)
         SparseMatrix<double> L( m * n , m * n );
         L.reserve( VectorXi::Constant( m * n , 8 ) );
-        //MatrixXd L( m * n , m * n );
         GraphLaplacianMatrix( I , betta , sigma , L );
-        tend = time(0);                                                                     // ***
-        cout << "L took "<< difftime(tend, tstart) <<" second(s)."<< endl;                  // ***
 
-        tstart = time(0);                                                                   // ***
         // Compute seeds dependent matrices (Is , b)
         SparseMatrix<double> Is( m * n , m * n );
         VectorXd b( m * n );
-        SeedsDependentMatrices( m , n, xf , xb , Is , b );
-        tend = time(0);                                                                     // ***
-        cout << "Is and b took "<< difftime(tend, tstart) <<" second(s)."<< endl;           // ***
+        b = VectorXd::Zero( m * n );
+        SeedsDependentMatrices( xf , xb , Is , b );
 
-        tstart = time(0);                                                                   // ***
         // Compute the Graph Laplacian Matrix square
-        GraphLaplacianMatrixSquare( m , n , L );
-        tend = time(0);                                                                     // ***
-        cout << "L^2 took "<< difftime(tend, tstart) <<" second(s)."<< endl;                // ***
+        SparseMatrix<double> L2( m * n , m * n );
+        L2.reserve( VectorXi::Constant( m * n , 8 ) );
+        L2 = GraphLaplacianMatrixSquare( L );
 
-        tstart = time(0);                                                                   // ***
+        const clock_t begin_time2 = clock();
         // Solve linear system
         VectorXd X( m * n );
-        ComputeLinearSystem( m , n , Is + L , b , X );
-        tend = time(0);                                                                     // ***
-        cout << "Ax = B took "<< difftime(tend, tstart) <<" second(s)."<< endl;             // ***
+        ComputeLinearSystem( Is + L , b , X );
+        cout << "Ax = b took " << float( clock () - begin_time2 ) /  CLOCKS_PER_SEC << endl;
 
-        tstart = time(0);                                                                   // ***
         // Assign labels to x
         Mat_<float> Y = Mat_<float>::zeros( m , n );
+        AssignLabels( m , n , xf , xb , X , Y );
 
-        // AssignLabels( m , n , xf , xb , X , Y );         // NOT WORKING... WHY?
+        // Print time tooken for segmentation process
+        cout << "Segmentation process took " << float( clock () - t_start ) /  CLOCKS_PER_SEC << endl;
 
-    // ///////////////////
-        // Threshold to assign labels
-        double threshold = ( ( xb + xf ) / 2 );
-
-        // Variable used to reshape from vector to matrix
-        int r = 0;
-
-        // Assign labels, reshape from vector to matrix and change from eigen to opencv
-        for ( int i = 0 ; i < m * n ; i++ ) {
-
-            if ( ( i - r * n ) == n ) {
-                r++;
-            }
-
-            if ( X( i ) >= threshold ) {
-                Y( r , i - r * n ) = float(xb);
-            }
-            else {                          // Depends on the value of xf this can be avoided
-                Y( r , i - r * n ) = float(xf);
-            }
-        }
-    // ///////////////////
-
-        //AssignLabels( m , n , xf , xb , X , Y );
-        tend = time(0);                                                                     // ***
-        cout << "Labels took "<< difftime(tend, tstart) <<" second(s)."<< endl;             // ***
-
-        tend = time(0);                                                                     // ***
-        cout << "Segmentation took "<< difftime(tend, globalstart) <<" second(s)."<< endl;             // ***
-
-        //**************************
-        //
+        //All this should be moved out from this CLASS
+// *****************************************************************************************************************************************************************
         // Show the segmented image
-        //
-        //**************************
-
-        // Show the segmented image
-
         //namedWindow( "SI" , CV_WINDOW_NORMAL );
-        //imshow( "SI" , Y );
+        imshow( "SI" , Y );
         //cout << "Y.cols" << Y.cols << endl;
         //cout << "Y.rows" << Y.rows << endl;
         //QPixmap dest= QPixmap((uchar*) Y.data, Y.cols, Y.rows, Y.step, QPixmap::Format_RGB888);
@@ -153,6 +118,7 @@ public slots:
         //cout << "q.width" << q.width << endl;
         //cout << "q.height" << q.height << endl;
         emit sendImage(q);
+// *****************************************************************************************************************************************************************
     }
 
 private:
@@ -174,14 +140,10 @@ private:
         // Initialize adjency matrix W with zeros
         SparseMatrix<double> W( m * n , m * n );
         W.reserve( VectorXi::Constant( m * n , 8 ) );                                             // 8 for normal neighborhood
-        //MatrixXd W( m * n , m * n );
-        //W = MatrixXd::Zero( m * n , m * n );
 
         // Initialize diagonal matrix D with zeros
         SparseMatrix<double> D( m * n , m * n );
         D.reserve( VectorXi::Constant( m * n , 1 ) );
-        //MatrixXd D( m * n , m * n );
-        //D = MatrixXd::Zero( m * n , m * n );
 
         // For each row                                                                           // --> TODO: Optimize the code changing the way to compute D
         for ( int i = 0 ; i < m ; i++ ) {                                                         //           (avoid compute twice the same W -> uncomment condition in below if)
@@ -222,18 +184,10 @@ private:
         L = D - W;
     }
 
-    void SeedsDependentMatrices( const int &m , const int &n , const int &xf , const int &xb , SparseMatrix<double> &Is , VectorXd &b ) {
-
-        // Initialize Is and b with zeros
-        //Is = MatrixXd::Zero( m * n , m * n );
-        b = VectorXd::Zero( m * n );
-
-        int a = 0;
-
+    void SeedsDependentMatrices( const int &xf , const int &xb , SparseMatrix<double> &Is , VectorXd &b ) {
         // Compute Is matrix and b vector                                                         // --> TODO: Apply OR gate? It can avoid go through all the seeds
         for( QSet< QPair< int, int> >::Iterator it = monitor->fgSeeds.begin(); it != monitor->fgSeeds.end(); ++it) {
-            a = it->first + it->second * monitor->image->width();
-            Is.coeffRef( a , a ) = 1;
+            Is.coeffRef( it->first + it->second * monitor->image->width() , it->first + it->second * monitor->image->width() ) = 1;
             b( it->first + it->second * monitor->image->width() ) = xf;
         }
         for( QSet< QPair< int, int> >::Iterator it = monitor->bgSeeds.begin(); it != monitor->bgSeeds.end(); ++it) {
@@ -242,25 +196,16 @@ private:
         }
     }
 
-    void GraphLaplacianMatrixSquare( const int &m , const int &n , Eigen::SparseMatrix<double> &L ) {
-        //Eigen::SparseMatrix<double> L_sparse( m * n , m * n );
-        Eigen::SparseMatrix<double> L2_sparse_test( m * n , m * n );
-        //L_sparse = L.sparseView();
-        //L2_sparse_test = L_sparse * L_sparse;
-        L2_sparse_test = L * L;
-        //L = MatrixXd(L2_sparse_test);
-        L = L2_sparse_test;
+    SparseMatrix<double> GraphLaplacianMatrixSquare( SparseMatrix<double> &L ) {
+        return ( L * L );
     }
 
-    void ComputeLinearSystem( const int &m , const int &n , const SparseMatrix<double> &Is_L , const VectorXd &b , VectorXd &X ) {
-        //SparseMatrix<double> A( m * n , m * n );
-        //A = Is_L.sparseView();
-
-        SimplicialCholesky< Eigen::SparseMatrix<double> > chol( Is_L );    // performs a Cholesky factorization of A
-        X = chol.solve( b );                       // uses the factorization to solve for the given right hand side
+    void ComputeLinearSystem( const SparseMatrix<double> &Is_L , const VectorXd &b , VectorXd &X ) {
+        SimplicialCholesky< SparseMatrix<double> > chol( Is_L );
+        X = chol.solve( b );
     }
 
-    void AssignLabels( const int &m , const int &n , const int &xf , const int &xb , const VectorXd &X , Mat_<double> &Y ) {
+    void AssignLabels( const int &m , const int &n , const double &xf , const double &xb , const VectorXd &X , Mat_<float> &Y ) {
         // Threshold to assign labels
         double threshold = ( ( xb + xf ) / 2 );
 
@@ -275,15 +220,16 @@ private:
             }
 
             if ( X( i ) >= threshold ) {
-                Y( r , i - r * n ) = xb;
+                Y( r , i - r * n ) = float(xb);
             }
-            else {                          // Depends on the value of xf this can be avoided
-                Y( r , i - r * n ) = xf;
+            else {                                                                          // Depends on the value of xf this can be avoided
+                Y( r , i - r * n ) = float(xf);
             }
         }
     }
 
 signals:
+
     void sendImage( QPixmap );
 };
 
