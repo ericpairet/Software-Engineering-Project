ee50ea8807b0e4b8a8ad1e6324233e83e29c5f1b
PaolaArdon <ardonp@hotail.com>
2016-01-09 11:13:58 +0100

    Adding comments for widgets (Doxygen)

--- CIS/widgets.h
+++ CIS/widgets.h
@@ -38,7 +38,8 @@
 void debug(QString text, QColor color = "black");
 
 /**
- * @brief
+ * @brief CTabDockWidget : class in charge of creating the tabs
+ * when a new seed is selected in the GUI
  *
  */
 class CTabDockWidget : public QDockWidget
@@ -46,13 +47,14 @@ class CTabDockWidget : public QDockWidget
     Q_OBJECT
 public:
     /**
-     * @brief
+     * @brief CTabDockWidget : function in charge of creating a new tab
+     * every time the user selects a new seed for multisegmentation
      *
-     * @param parent
+     * @param parent : constructing a QDockWidget with parent parent
      */
     CTabDockWidget(QWidget* parent);
     /**
-     * @brief
+     * @brief ~CTabDockWidget : Destructor member function for CTabDockWidget
      *
      */
     ~CTabDockWidget();
@@ -60,7 +62,7 @@ public:
     QWidget* w; /**< TODO */
 signals:
     /**
-     * @brief
+     * @brief resized : ???? WHERE IS IT ??
      *
      * @param QSize
      */
@@ -68,7 +70,12 @@ signals:
 };
 
 /**
- * @brief
+ * @brief : CToolsWidged : class in charge of handling the tools
+ * needed to manage the GUI. It handles the functions of updating
+ * the input image selected by the user (as QPixmap and QImage),the
+ * function in charge of converting the beta value given by the slider
+ * into double so that it can be used in the segmentation code, and
+ * the function in charge of resizing the input image.
  *
  */
 class CToolsWidget : public QWidget
@@ -76,13 +83,17 @@ class CToolsWidget : public QWidget
     Q_OBJECT
 public:
     /**
-     * @brief
+     * @brief CToolsWidget : function in charge of handling the size
+     * postion and labels of the different push-buttons,line
+     * inputs and slider in the GUI. It connects the push-buttons
+     * loadButton and bethaSlider with the funtions loadSlot and
+     * updateBethaValue respectevely.
      *
-     * @param parent
+     * @param parent : constructing a QDockWidget with parent parent
      */
     CToolsWidget(QWidget *parent = 0);
     /**
-     * @brief
+     * @brief ~CToolsWidget : Destructor member function for CToolsWidget
      *
      */
     ~CToolsWidget();
@@ -96,33 +107,37 @@ private:
     QSize imageLimit; /**< TODO */
 private slots:
     /**
-     * @brief
+     * @brief loadSlot : function in charge of getting the input image selected
+     * by the user by following a string. It emits the signals:
+     * imageLoadedQPixmap and imageLoadedQImage.
      *
      */
     void loadSlot();
     /**
-     * @brief
+     * @brief updateBethaValue : function in charge of converting the value given
+     * by the slider into a decimal value which is then used in the segmentation
+     * code
      *
-     * @param _val
+     * @param _val : value given by the slider in the GUI
      */
     void updateBethaValue(int _val);
 signals:
     /**
-     * @brief
+     * @brief imageLoadedQPixmap :
      *
      * @param QPixmap
      * @param bool
      */
     void imageLoadedQPixmap( QPixmap, bool);
     /**
-     * @brief
+     * @brief imageLoadedQImage :
      *
      * @param QImage
      */
     void imageLoadedQImage( QImage);
 public slots:
     /**
-     * @brief
+     * @brief setImageSizeLimit :
      *
      * @param QSize
      */
@@ -130,7 +145,9 @@ public slots:
 };
 
 /**
- * @brief
+ * @brief CMonitorWidget : class in charge of handling showing the images
+ * in the monitor. It contains the following functions: setting and updating the
+ * image on the screen; and painting, setting and clearing the selected seeds.
  *
  */
 class CMonitorWidget : public QWidget
@@ -138,14 +155,14 @@ class CMonitorWidget : public QWidget
     Q_OBJECT
 public:
     /**
-     * @brief
+     * @brief CMonitorWidget :
      *
      * @param _tools
      * @param parent
      */
     explicit CMonitorWidget( CToolsWidget *_tools, QWidget *parent = 0);
     /**
-     * @brief
+     * @brief CMonitorWidget : Destructor member function for CMonitorWidget
      *
      */
     ~CMonitorWidget();
@@ -160,24 +177,30 @@ private:
     bool dragging; /**< TODO */
 public slots:
     /**
-     * @brief
+     * @brief updateImage : function in charge of updating the image in
+     * the GUI. Every time the user selects a new image this function shows
+     * the new image in the first tab of the application.
      *
-     * @param QPixmap
+     * @param QPixmap : image selected by the user. The parameter is gotten from
+     * imageLoadedQPixmap signal which sends the QPixmap to be read.
+     *
      */
     void updateImage( QPixmap);
     /**
-     * @brief
+     * @brief clearAllSeeds : function in charge of deleting the painted seeds of
+     * the previous image when the user selects a new one to be segmented.
+     *
      *
      */
     void clearAllSeeds();
     /**
-     * @brief
+     * @brief setInputImage :
      *
      * @param _p
      */
     inline void setInputImage( QPixmap _p){ image = new QPixmap( _p);}
     /**
-     * @brief
+     * @brief setCurrentSeed :  ??? WHERE IS IT???
      *
      * @param _s
      * @param _c
@@ -185,20 +208,22 @@ public slots:
     inline void setCurrentSeed( QString _s, QColor _c){ seedName = _s; seedColor = _c; seedsColor[seedName] = seedColor;}
 private slots:
     /**
-     * @brief
+     * @brief paintEvent : function in charge of painting the seeds in the selected
+     * image ->......???
      *
      * @param
      */
     void paintEvent(QPaintEvent *);
     /**
-     * @brief
+     * @brief mouseMoveEvent : it follows the position of the mouse to draw
+     * the seeds on the selected image ->.... ???
      *
      * @param event
      */
     void mouseMoveEvent(QMouseEvent *event);
 signals:
     /**
-     * @brief
+     * @brief imageLoaded1 : ??? WHERE IS IT ???
      *
      * @param QPixmap
      */
@@ -206,7 +231,7 @@ signals:
 };
 
 /**
- * @brief
+ * @brief CViewerWidget : tabs...?
  *
  */
 class CViewerWidget : public QTabWidget
@@ -214,26 +239,26 @@ class CViewerWidget : public QTabWidget
     Q_OBJECT
 public:
     /**
-     * @brief
+     * @brief CViewerWidget :
      *
      * @param parent
      */
     CViewerWidget( QWidget *parent = 0);
     /**
-     * @brief
+     * @brief ~CViewerWidget : Destructor member function for CViewerWidget
      *
      */
     ~CViewerWidget();
 private:
     /**
-     * @brief
+     * @brief resizeEvent :
      *
      * @param event
      */
     void resizeEvent(QResizeEvent *event);
 signals:
     /**
-     * @brief
+     * @brief imageSizeLimitSignal :
      *
      * @param QSize
      */
@@ -241,7 +266,7 @@ signals:
 };
 
 /**
- * @brief
+ * @brief CSeedWidget :
  *
  */
 class CSeedWidget : public QWidget
@@ -249,7 +274,7 @@ class CSeedWidget : public QWidget
     Q_OBJECT
 public:
     /**
-     * @brief
+     * @brief CSeedWidget :
      *
      * @param name
      * @param gp
@@ -257,18 +282,20 @@ public:
      */
     CSeedWidget( QString name, QButtonGroup *gp, QWidget *parent = 0);
     /**
-     * @brief
+     * @brief ~CSeedWidget : Destructor member function for CSeedWidget
      *
      */
     ~CSeedWidget();
     /**
-     * @brief
+     * @brief getColor : inline function in charge of getting the color selected by the user
+     * to paint the seeds in the input image. Different colors correspond to different seeds,
+     * this in order to differentiate between background and foreground seeds.
      *
-     * @return QColor
+     * @return QColor :
      */
     inline QColor getColor(){ return seedColor;}
     /**
-     * @brief
+     * @brief getName : ??? WHERE IS IT??
      *
      * @return QString
      */
@@ -286,14 +313,14 @@ private slots:
      */
     void setColor();
     /**
-     * @brief
+     * @brief emitSeedChanged :
      *
      * @param is
      */
     void emitSeedChanged( bool is);
 signals:
     /**
-     * @brief
+     * @brief seedChanged :
      *
      * @param QString
      * @param QColor
@@ -310,13 +337,13 @@ class CSeedSelectionWidget : public QWidget
     Q_OBJECT
 public:
     /**
-     * @brief
+     * @brief CSeedSelectionWidget :
      *
      * @param parent
      */
     CSeedSelectionWidget( QWidget *parent = 0);
     /**
-     * @brief
+     * @brief ~CSeedSelectionWidget : Destructor member function for CSeedSelectionWidget
      *
      */
     ~CSeedSelectionWidget();
@@ -327,7 +354,7 @@ private:
     QButtonGroup *buttonsGroup; /**< TODO */
 public slots:
     /**
-     * @brief
+     * @brief removeSeeds :
      *
      */
     void removeSeeds();
@@ -338,7 +365,7 @@ private slots:
      */
     void addNewSeed();
     /**
-     * @brief
+     * @brief emitSelectedSeed :
      *
      * @param _n
      * @param _c
@@ -346,7 +373,7 @@ private slots:
     void emitSelectedSeed( QString _n, QColor _c);
 signals:
     /**
-     * @brief
+     * @brief selectedSeedSignal :
      *
      * @param QString
      * @param QColor
@@ -356,14 +383,14 @@ signals:
 
 
 /**
- * @brief
+ * @brief CStatusText :
  *
  */
 class CStatusText
 {
     public:
     /**
-     * @brief
+     * @brief CStatusText :
      *
      * @param _text
      * @param _color
@@ -382,14 +409,14 @@ class CStatusText
 };
 
 /**
- * @brief
+ * @brief CStatusPrinter :
  *
  */
 class CStatusPrinter
 {
     public:
     /**
-     * @brief
+     * @brief CStatusPrinter :
      *
      */
     CStatusPrinter() {}
@@ -397,7 +424,7 @@ class CStatusPrinter
 };
 
 /**
- * @brief
+ * @brief CStatusWidget :
  *
  */
 class CStatusWidget : public QDockWidget
@@ -405,7 +432,7 @@ class CStatusWidget : public QDockWidget
     Q_OBJECT
 public:
     /**
-     * @brief
+     * @brief CStatusWidget :
      *
      * @param _statusPrinter
      */
@@ -420,19 +447,19 @@ public:
     QTextDocument content; /**< TODO */
 public slots:
     /**
-     * @brief
+     * @brief write :
      *
      * @param str
      * @param color
      */
     void write(QString str, QColor color = QColor("black"));
     /**
-     * @brief
+     * @brief update :
      *
      */
     void update();
     /**
-     * @brief
+     * @brief getSizeFromViewer :
      *
      * @param size
      */
@@ -442,14 +469,14 @@ private:
     QTime logTime; /**< TODO */
 signals:
     /**
-     * @brief
+     * @brief closeSignal :
      *
      * @param bool
      */
     void closeSignal(bool);
 protected:
     /**
-     * @brief
+     * @brief closeEvent :
      *
      * @param
      */
